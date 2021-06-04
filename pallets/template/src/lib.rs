7abd519224d1edda46534d6b14fe0902ff771013
pooja-anandani <poojaa358@gmail.com>
2021-06-04 16:34:28 +0530

    custome-pallet

---
+++ pallets/template/src/lib.rs
@@ -0,0 +1,66 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet{
+	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+	use frame_system::pallet_prelude::*;
+	use sp_std::vec::Vec; 
+	#[pallet::config] 
+	pub trait Config: frame_system::Config {
+		type Event : From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+	#[pallet::event]
+	#[pallet::metadata(T::AccountId = "AccountId")]
+  #[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config>{
+		ClaimCreated(T::AccountId, Vec<u8>),
+		ClaimRevoked(T::AccountId, Vec<u8>),
+	 }
+	#[pallet::error]
+	pub enum Error<T> {
+		ProofAlreadyClaimed,
+		NoSuchProofExists,
+		NotProofOwner,
+	}
+	#[pallet::pallet]
+  #[pallet::generate_store(pub(super) trait Store)]
+  pub struct Pallet<T>(_);
+
+	#[pallet::storage] 
+  pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;   
+	#[pallet::hooks]
+  impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+	#[pallet::call]
+  impl<T: Config> Pallet<T> {
+		#[pallet::weight(1_000)]
+		pub(super) fn create_claim(origin: OriginFor<T>, proof: Vec<u8>,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+			let current_block = <frame_system::Module<T>>::block_number();
+			Proofs::<T>::insert(&proof,(&sender, current_block));
+			Self::deposit_event(Event::ClaimCreated(sender, proof));
+			Ok(().into())
+		}
+		#[pallet::weight(10_000)]
+		fn revoke_claim(
+			origin:OriginFor<T>,
+			proof: Vec<u8>,
+		) -> DispatchResultWithPostInfo{
+			let sender = ensure_signed(origin)?;
+			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProofExists);
+			let (owner, _) = Proofs::<T>::get(&proof);
+			ensure! (sender == owner, Error::<T>::NotProofOwner);
+			Proofs::<T>::remove(&proof);
+			Self::deposit_event(Event::ClaimRevoked(sender,proof));
+			Ok(().into())
+		}
+	}
+}
+
